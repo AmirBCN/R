5a58dd69b96f8f00493ef60d8b10bc211e344a9f
AmirBCN <amirjapan2007@gmail.com>
2018-03-18 13:02:47 +0100

    First commit

---
+++ pollutantmean.R
@@ -0,0 +1,25 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  SUM <- 0
+  LG <- 0
+  for(i in id)
+  {
+    csvfiles <- sprintf("%s/%03d.csv", directory, i)
+    Data <- read.csv(file=csvfiles, sep=",")
+    
+    #for(j in 1:nrow(Data[pollutant]))
+    #{
+    #  if(is.na(Data[j,pollutant]) == FALSE)
+    #  {
+    #    SUM <- SUM + Data[j,pollutant]
+    #    LG <- LG + 1
+    #  }
+    #}
+    
+    DataClean <- na.omit(Data[pollutant])
+    SUM <- SUM + as.numeric(colSums(DataClean))
+    LG <- LG + nrow(DataClean)
+  }
+  SUM / LG
+}
+
+  
